fb8d315edddf9c1926a6132e28e88a680cc8d512
joseph lee <hijosephl31@gmail.com>
2026-05-22 01:59:13 -0400

    Add: is-anagram - submission-1

---
+++ Data Structures & Algorithms/is-anagram/submission-1.py
@@ -0,0 +1,9 @@
+from collections import defaultdict
+
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False
+        else:
+            return sorted(s) == sorted(t)
+        
